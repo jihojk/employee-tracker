8255f80b5e533388861406e8e6a6f62abe63df85
Jiho Kim <jihojk@uci.edu>
2020-02-16 21:42:00 -0800

    created directories and files

---
+++ server.js
@@ -0,0 +1,20 @@
+// requires
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+const cTable = require('console.table');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "stupidSql",
+    database: ""
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+})
+
+app.listen(PORT, function() {
+    console.log("server listening on: http://localhost:" + PORT)
+})
